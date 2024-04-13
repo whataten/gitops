b0061239f60a126d5cf281d0cda385158c654439
whataten <tiktaalik135462@gmail.com>
2024-04-13 07:58:56 +0000

    Create basic project

---
+++ src/test/java/ten/gitops/GitopsApplicationTests.java
@@ -0,0 +1,13 @@
+package ten.gitops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

7f0e175ee5ceb87d7dae8f70cd3146ae2cf68f09
whataten <tiktaalik135462@gmail.com>
2024-07-26 10:17:04 +0900

    Test ECR_image_tag:0.0.6

--- src/main/java/ten/gitops/GitopsApplication.java
+++ src/main/java/ten/gitops/GitopsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitopsApplication {
 
 	public static void main(String[] args) {
+		System.out.println("RUNNING");
 		SpringApplication.run(GitopsApplication.class, args);
 	}
-
 }
